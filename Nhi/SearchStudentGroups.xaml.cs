4bcca25336d89d5d7976cbf25504f358c1a9ece6
Nirash Jude <84084879+Nirashjude@users.noreply.github.com>
2022-05-11 20:30:37 +1000

    Add files via upload

---
+++ Nhi/SearchStudentGroups.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GMIS___Assignment_03.Nhi
+{
+    /// <summary>
+    /// Interaction logic for SearchStudentGroups.xaml
+    /// </summary>
+    public partial class SearchStudentGroups : Window
+    {
+        public SearchStudentGroups()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainPage main_page = new MainPage();
+            main_page.Show();
+            this.Hide();
+        }
+    }   
+}
