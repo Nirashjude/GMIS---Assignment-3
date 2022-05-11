de1e3b3260d669705b79cbf560cc76422f4a71f9
Nirash Jude <84084879+Nirashjude@users.noreply.github.com>
2022-05-11 20:13:52 +1000

    Add files via upload

---
+++ Ashley/ViewAllGroup.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace GMIS___Assignment_03.Ashley
+{
+    /// <summary>
+    /// Interaction logic for ViewAllGroup.xaml
+    /// </summary>
+    public partial class ViewAllGroup : Window
+    {
+        public ViewAllGroup()
+        {
+            InitializeComponent();
+            string connectionString = "Database=gmis;Data Source=alacritas.cis.utas.edu.au;User Id=gmis;Password=gmis";
+            var connection = new MySqlConnection(connectionString);
+            var command = new MySqlCommand("select * from studentGroup", connection);
+            connection.Open();
+            DataTable dt = new DataTable();
+            dt.Load(command.ExecuteReader());
+            connection.Close();
+            dtGrid.DataContext = dt;
+        }
+
+           
+        }
+}
---
+++ Ashley/ViewAllStudent.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GMIS___Assignment_03.Ashley
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ViewAllStudent : Window
+    {
+        public ViewAllStudent()
+        {
+            InitializeComponent();
+            string connectionString = "Database=gmis;Data Source=alacritas.cis.utas.edu.au;User Id=gmis;Password=gmis";
+            var connection = new MySqlConnection(connectionString);
+            var command = new MySqlCommand("select * from student", connection);
+            connection.Open();
+            DataTable dt = new DataTable();
+            dt.Load(command.ExecuteReader());
+            connection.Close();
+            dtstudent.Datacontext = dt;
+        }
+
+      
+    }
+    }
+    
+}
