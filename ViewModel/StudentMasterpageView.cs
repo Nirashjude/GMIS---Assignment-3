48a21054e7c3bc5c3d079b320927381cb3eee40e
Nirash Jude <84084879+Nirashjude@users.noreply.github.com>
2022-05-11 20:46:27 +1000

    Add files via upload

---
+++ ViewModel/StudentMasterpageView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMIS___Assignment_03.ViewModel
+{
+    Public class StudentMasterpageView
+    {
+        public List<StudentMAstepageView> StudentMasterpageViews{ get; set; }
+
+    }
+}
---
+++ ViewModel/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GMIS___Assignment_03.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMIS___Assignment_03.ViewModel
+{
+    public class StudentViewModel
+    {
+        public List<Student> Students { get; set; }
+    }
+}
